b5427685584edd304c501c10281a69a4d98bcde1
Peter F. Guenther <peter.guenther@gmail.com>
2018-05-22 15:17:19 -0400

    first commit

---
+++ CarApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarApp.Models;
+
+namespace CarApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Car> carList = new List<Car>();
+            carList.Add(new Car("Pontiac", "Aztek", 2007));
+            carList.Add(new Car("Tesla", "S3", 2018));
+            carList.Add(new Car("Fiat", "500", 2016));
+
+            ViewBag.CarList = carList;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult CarForm()
+        {
+            Car car = new Car();
+            ViewBag.Car = car;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Create(Car c)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Car = c;
+                return View();
+            }
+            else
+            {
+                return View("CarForm");
+            }
+            
+        }
+    }
+}
---
+++ CarApp/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarApp.Models
+{
+    public class Car
+    {
+        private string make;
+        private string plate;
+        private int year;
+
+        public Car()
+        {
+            make = "";
+            plate = "";
+            year = 1907;
+        }
+
+        public Car(string make, string plate, int year)
+        {
+            this.make = make;
+            this.plate = plate;
+            this.year = year;
+        }
+
+        public string Make { get => make; set => make = value; }
+        public string Plate { get => plate; set => plate = value; }
+        public int Year { get => year; set => year = value; }
+    }
+}
